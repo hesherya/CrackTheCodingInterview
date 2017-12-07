d2c024597838e489cc0b860fd400d4941942a80a
Hui Wang <hedonplay@gmail.com>
2017-12-07 23:17:44 +0100

    remove tests

--- src/test/java/arraystring/_01_01_IsUniqueTest.java
+++ src/test/java/arraystring/_01_01_IsUniqueTest.java
@@ -2,26 +2,13 @@ package arraystring;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 public class _01_01_IsUniqueTest {
 
     private _01_01_IsUnique s = new _01_01_IsUnique();
 
     @Test
-    public void withEmptyString() {
-        assertTrue(s.isUnique(""));
+    public void test() {
     }
 
-    @Test
-    public void withUniqueString() {
-        assertTrue(s.isUnique("abcde"));
-    }
-
-    @Test
-    public void withDuplication() {
-        assertFalse(s.isUnique("abcda"));
-    }
 
 }
